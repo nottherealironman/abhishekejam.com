7230f6ad3fdb092b2bfdc482d31f2ece6b21ef5b
nottherealironman <ejam.abhishek@gmail.com>
2023-11-07 11:29:04 +1100

    fix: mobile nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,22 +17,6 @@ $(document.body).on('click', '.mobile-nav-toggler', function (e) {
     }
 });
 
-$(document).on('click', '.close', function (e) {
-    $(this).closest('.notification').remove();
-    localStorage.setItem('notification', false);
-});
-
-$(document.body).on('click', '.mobile-nav-toggler', function(e){
-    $('.right-sidebar').toggle();
-    if($(document.body).hasClass('disabled')){
-        $(document.body).addClass('enabled').removeClass('disabled');
-        $(e.currentTarget).find('.fa').addClass('fa-bars').removeClass('fa-close');
-    }
-    else{
-        $(document.body).addClass('disabled').removeClass('enabled');
-        $(e.currentTarget).find('.fa').removeClass('fa-bars').addClass('fa-close');
-    }
-});
 
 $(document.body).on('click', '.close', function(e){
     var target = $(e).currentTarget;
